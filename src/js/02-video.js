20ef329240e9d5d6967e24c1754bc80cd6091deb
RomanProtsenko <126424199+RomanProtsenko@users.noreply.github.com>
2023-08-10 22:41:13 +0300

    3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
+
+function getCurrentTime (currentTime) {
+  const seconds = currentTime.seconds;
+  localStorage.setItem(CURRENT_TIME, JSON.stringify(seconds));
+};
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+window.addEventListener('DOMContentLoaded', setCurrentTime);
+
+function setCurrentTime() {
+  const currentTime = localStorage.getItem(CURRENT_TIME);
+  if (currentTime) {
+    player.setCurrentTime(JSON.parse(currentTime));
+  }
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const STORAGE_KEY = 'feedback-form-state';
+
+function saveFormData() {
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function populateFormFields() {
+  const savedFormData = localStorage.getItem(STORAGE_KEY);
+  if (savedFormData) {
+    const { email, message } = JSON.parse(savedFormData);
+    emailInput.value = email;
+    messageInput.value = message;
+  } else {
+    emailInput.value = '';
+    messageInput.value = '';
+  }
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  const formData = {
+    email: emailInput.value,
+    message: messageInput.value
+  };
+  if (emailInput.value.trim() === "" || messageInput.value.trim() === "") {
+      return alert("All data must be filled");
+    } 
+  console.log(formData);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
+
+const throttledSaveFormData = throttle(saveFormData, 500);
+
+form.addEventListener('submit', onSubmit);
+emailInput.addEventListener('input', throttledSaveFormData);
+messageInput.addEventListener('input', throttledSaveFormData);
+
+populateFormFields();
